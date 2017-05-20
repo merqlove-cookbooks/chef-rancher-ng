e3e604e8fbefe3e8bea9f8790bc7adfc8716224c
Alexander Merkulov <sasha@merqlove.ru>
2017-05-20 18:48:14 +0300

    Bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sasha@merqlove.ru'
 license          'Apache 2.0'
 description 'Installs/Configures Rancher'
 long_description 'Installs/Configures Rancher'
-version '0.1.3'
+version '0.1.4'
 
 depends 'docker', '~> 2.0'
 
