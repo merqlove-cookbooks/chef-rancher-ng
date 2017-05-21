bb7d455e73f30a506ac5aaca4bc653698ceace57
Alexander Merkulov <sasha@merqlove.ru>
2017-05-21 11:13:20 +0300

    Fix url

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sasha@merqlove.ru'
 license          'Apache 2.0'
 description 'Installs/Configures Rancher'
 long_description 'Installs and configures Rancher service'
-version '0.1.7'
+version '0.1.8'
 
 depends 'docker', '~> 2.0'
 
@@ -12,10 +12,10 @@ depends 'docker', '~> 2.0'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-issues_url 'https://github.com/merqlove-cookbooks/rancher-ng/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/merqlove-cookbooks/chef-rancher-ng/issues' if respond_to?(:issues_url)
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-source_url 'https://github.com/merqlove-cookbooks/rancher-ng' if respond_to?(:source_url)
+source_url 'https://github.com/merqlove-cookbooks/chef-rancher-ng' if respond_to?(:source_url)
