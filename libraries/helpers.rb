5fc5a2ffde82d9c293f1191338cda532ab3b3390
Alexander Merkulov <sasha@merqlove.ru>
2017-05-21 12:36:56 +0300

    Easier print

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,10 +1,10 @@
 module RancherNg
   module Helpers
     def print_json(new_resource)
-      [:name, :image, :version, :detach, :restart_policy, :port, :db_dir, :db_host, :db_port, :db_user, :db_pass, :db_name].reduce([]) do |acc, key|
-        acc << "#{key}: #{ new_resource.send(key).to_s }"
+      [:name, :image, :version, :detach, :restart_policy, :port, :db_dir, :db_host, :db_port, :db_user, :db_pass, :db_name].reduce("\n") do |acc, key|
+        acc << "#{key}: #{ new_resource.send(key).to_s }\n"
         acc
-      end.join("\n")
+      end
     end
 
     def debug_resource(new_resource)
